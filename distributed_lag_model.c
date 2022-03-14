5315c316f4307304a7fb483747bdc0f3d0b4c7ab
MinMagar <magarhomeboy@gmail.com>
2022-03-14 14:50:48 +0545

    two lab file complete

---
+++ distributed_lag_model.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main () {
+
+    float I[100], C[100], T[100], G[100], Y[50];
+    int no_of_years, i;
+
+    printf("Enter the National income for 2021: ");
+    scanf("%f", &Y[0]);
+    printf("Enter the Government Expenditure for 2021: ");
+    scanf("%f", &G[0]);
+    printf("Enter Number of Years to Simulate: ");
+    scanf("%d", &no_of_years);
+
+    printf("\n Income[2021]: %.2f \n Government_Expenditure[2021]: %.2f \n", Y[0], G[0]);
+    printf("\n SN\t Years\t Investment(I)\t Tax(T)\t\t Expenditure(G)\t\t Consumption(C)\t\t Income(Y)\n");
+
+    for (i=1; i <= no_of_years; i++) {
+        I[i] = 2 + 0.1 * Y[i-1];
+        T[i] = 0.2 * Y[i-1];
+        G[i] = 0.15 * G[i-1] + G[i-1];
+        C[i] = 45.45 + 2.27 * (I[i] + G[i]);
+        Y[i] = C[i] + I[i] + G[i];
+        printf(" %d\t %d\t %.2f\t\t %.2f\t\t %.2f\t\t\t %.2f\t\t\t %.2f\t\n", i, 2021+i, I[i], T[i], G[i], C[i], Y[i]);
+    }
+
+    return 0;
+
+}
---
+++ monte_carlo.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<math.h>
+
+float function(float x, float y){
+    return (x * x + y * y);
+}
+
+int main(){
+    float rand_x[200], rand_y[200];
+    int x[200],y[200];
+    int m,X[20],Y[20],count[10];
+    int counter = 0,n = 10;
+    float pi[10],pi_avg;
+    printf("Enter modulous value : ");
+    scanf("%d",&m);
+    int index=0;
+    for (int i = 0;i < 2; i++){
+        printf("Enter %d value of X[%d] and Y[%d]: ",i,(i%2)+1,(i%2)+1);
+        scanf("%d%d",&X[index],&Y[index]);
+        printf("Enter %d value of X[%d] and Y[%d]: ",i,(i%2)+2,(i%2)+2);
+        scanf("%d%d",&X[index+1],&Y[index+1]);
+        index += 2;
+    }
+    index = 0;
+    for (int i = 0; i < n; i++){
+        x[0] = (float)X[index];
+        y[0] = (float)Y[index];
+        x[1] = (float)X[index + 1];
+        y[1] = (float)y[index + 1];
+        for (int k=2;k<200;k++){
+            y[k] = (y[k-1]+y[k-2])%m;
+            x[k] = (x[k-1]+x[k-2])%m;
+        }
+        for (int k = 0;k < 200; k++){
+            rand_x[k] = (float)x[k]/m;
+            rand_y[k] = (float)y[k]/m;
+        }
+        for (int k = 0;k < 200;k++){
+            if(function(rand_x[k],rand_y[k]) <= 1){
+                counter++;
+            }
+        }
+        count[i] = counter;
+        pi[i] = (counter/200.0)*4.0;
+        counter = 0;
+        index += 2;
+    }
+    printf("\nrun\tx[0]\tx[1]\ty[0]\ty[1]\tn\tN\tpi");
+    index = 0;
+    for(int i=0; i<n; i++){
+        printf("\n%d\t%d\t%d\t%d\t%d\t%d\t200\t%f",i,X[index],X[index+1],Y[index],Y[index+1],count[i],pi[i]);
+        index += 2;
+    }
+    pi_avg = 0;
+    for (int i=0; i<10; i++){
+        pi_avg += pi[i];
+    }
+    pi_avg = pi_avg / 10;
+    float err = 0;
+    err = (fabs(pi_avg - 3.141)/3.141)*100.0;
+    printf("\nCalculated pi value is %f",pi_avg);
+    printf("\nError percentage is %f",err);
+
+    return 0;
+}
