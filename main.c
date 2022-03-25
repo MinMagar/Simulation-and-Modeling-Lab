d117641e75b7edbf0c436107dcdbf3b5c9d19b4c
MinMagar <magarhomeboy@gmail.com>
2022-03-25 18:37:58 +0545

    adding space for good display

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
     float* r;
     r = generate_random_float(2,4,100);
     for (int i = 0; i<100; i++){
-        printf("%f",r[i]);
+        printf("%f   ",r[i]);
         if ((i+1) % 10 == 0) {
             printf("\n");
         }
